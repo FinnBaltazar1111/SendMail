46d91f23e5989f1ad98b96c40ebfd3e5c08dc0c9
s0urce-c0de <email.kanavg@gmail.com>
2023-08-03 20:02:18 -0700

    added files

--- send_mail.py
+++ send_mail.py
@@ -1,50 +1,45 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.12
 # -*- encoding: utf-8 -*-
-"""
-Send mail from python
-"""
+'''
+***REQUIRES***
+Python==3.12
+'''
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import smtplib
 import click
-import requests
-name=__name__
-del __name__
+import sys
+import validators
+import socket
+
+if sys.version_info < (3, 12): raise RuntimeError("Requires Python 3.12a0+")
+
 # dummy class
 class Server(str): pass
 class Port(int):
   class PortOutOfRangeError(ValueError): pass
   def __init__(self, port: int):
+    port=int(port)
     if port <= 0 or port >= 65535:
-      raise self.PortOutOfRangeError(f"Port {port} is not in range of 0-65535 (inclusive)")
+      raise self.PortOutOfRangeError(f'Port {port} is not in range of 0-65535 (inclusive)')
 
-@click.command()
-@click.option('--argument', callback=get_default_value)
-@click.option('--arg2', is_flag=True)
-def my_command(argument, arg2):
-    click.echo(f"Argument: {argument}")
-
-if __name__ == '__main__':
-    my_command()
-
-@click.command(context_settings=dict(help_option_names=["-h", "--help"]))
-@click.option('--smtp-server', '-s', type=Server, required=True, help="SMTP Server to connenvt to. e.g. smtp.gmail.com, smtp-mail.outlook.com")
+@click.command(context_settings=dict(help_option_names=['-h', '--help']))
+@click.option('--smtp-server', '-s', type=Server, required=True, help='SMTP Server to connenvt to. e.g. smtp.gmail.com, smtp-mail.outlook.com')
 @click.option('--ssl/--tls', is_flag=True, help='Establish SSL or TLS', default=False)
-@click.option('--smtp-port', '-p', type=Port, callback=(lambda ctx, param, value: 465 if ctx.params['ssl'] else 587 if value is None else value))
+@click.option('--smtp-port', '-p', type=Port, callback=lambda ctx, param, value: (465 if ctx.params['ssl'] else 587) if value is None else value)
+@click.option('--email', '-e', type=str, prompt_callback=lambda ctx, param, value: f'Enter email for {ctx.params["smtp_server"]} ({socket.getbyhostname(ctx.params["smtp_server"])})')
+@click.option('-p', '--password', prompt='Enter your password: ', hide_input=True, confirmation_prompt='Confirm Your password', help='Password for SMTP Server', callback=lambda ctx, param, value: click.prompt(f'Enter password for') if value is None else value)
 
-#@click.option('--sender', '', )
-def main(smtp_server: str, smtp_port: int, ssl):
-  """
+def main(smtp_server: str, smtp_port: int, ssl: bool, email: str, password: str):
+  '''
   Send mail via SMTP Servers, from python!
-  """
-  sender = "ekanav@hotmail.com"
-  password = "Kanav@123"
-  reciever = "email.kanavg@gmail.com"
-  message = MIMEMultipart("alternative")
-  msg_text = requests.get("https://report3.live.fxinside.net/birt/jsp/reportApi.jsp?type=Volume&groupBy1=OrgLongName&format=html").text
+  '''
+  reciever = 'email.kanavg@gmail.com'
+  message = MIMEMultipart('alternative')
+  msg_text = "<i>italic</i>"
 
-  m1=MIMEText(msg_text, "plain")
-  m2=MIMEText(msg_text, "html")
+  m1=MIMEText(msg_text, 'plain')
+  m2=MIMEText(msg_text, 'html')
 
   message.attach(m1)
   message.attach(m2)
@@ -53,9 +48,9 @@ def main(smtp_server: str, smtp_port: int, ssl):
   else:
     server = smtplib.SMTP(smtp_server, port=int(smtp_port))
     server.starttls()
-  server.login(sender, password)
-  server.sendmail(sender, reciever, message.as_string())
+  server.login(email, password)
+  server.sendmail(email, reciever, message.as_string())
   server.quit()
 
-if name == "__main__":
+if __name__ == '__main__':
   main()
